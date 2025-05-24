efd1dfb0a879c30219fb3f45deb5236bee4be320
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-05-24 04:23:48 +0100

    Implment favorites list features

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -5,49 +5,74 @@ import DOMPurify from "dompurify";
 export const RecipeDetails = () => {
   const fullRecipe = useRecipeStore((state) => state.fullRecipe);
   const food = useRecipeStore((state) => state.food);
+  const loading = useRecipeStore((state) => state.loading);
+  const error = useRecipeStore((state) => state.error);
+  const setFavoriteRecipe = useRecipeStore((state) => state.setFavoriteRecipe);
 
   const { id } = useParams();
 
   return (
     <>
-      <p>Recipe ID: {id}</p>
-      <h1>Food Type: {food}</h1>
-      <h3>Recipe Title: {fullRecipe.title}</h3>
-      <img src={fullRecipe.image} alt={fullRecipe.title} />
-      <h3>Ingredients</h3>
-      {/* {fullRecipe.extendedIngredients.map((ingredient) => (
-        <ul key={ingredient.id}>
-          <li key={ingredient.id}>{ingredient.original}</li>
-        </ul>
-      ))} */}
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: ${error}</p>}
+      {fullRecipe && (
+        <>
+          <p>Recipe ID: {id}</p>
+          <h1>Food Type: {food}</h1>
+          <h3>Recipe Title: {fullRecipe.title}</h3>
+          <img src={fullRecipe.image} alt={fullRecipe.title} />
+          <div>
+            <h3>Ingredients</h3>
+            <ul>
+              {fullRecipe.extendedIngredients.map((ingredient) => (
+                <li key={crypto.randomUUID()}>{ingredient.original}</li>
+              ))}
+            </ul>
+          </div>
 
-      <div>
-        <p>Instructions:</p>
-        {fullRecipe.instructions ?? "No Instructions"}
-      </div>
-      <article>
-        <h1>Diets</h1>
-        {/* {fullRecipe.diets.map((diet) => (
-          <div>
-            <p>{diet}</p>
-          </div>
-        ))} */}
-        <h1>Dish Types</h1>
-        {/* {fullRecipe.dishTypes.map((dish) => (
-          <div>
-            <p>{dish}</p>
-          </div>
-        ))} */}
-        <div className="otherInfo">
-          <span>Ready In Minutes: {fullRecipe.readyInMinutes}min</span>
-        </div>
-        <h2>Summary</h2>
-        <p dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(fullRecipe.summary)}} />
-        <p></p>
-      </article>
-      <a href={fullRecipe.sourceUrl} target="_blank">
-        Read More about the recipe
-      </a>
+          <article>
+            <h3>Instructions:</h3>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  fullRecipe.instructions || "No Instructions"
+                ),
+              }}
+            />
+          </article>
+          <article>
+            <h1>Diets</h1>
+            <div>
+              {fullRecipe.diets.map((diet) => (
+                <p key={crypto.randomUUID()}>{diet}</p>
+              ))}
+            </div>
+            <h1>Dish Types</h1>
+            <div>
+              {fullRecipe.dishTypes.map((dish) => (
+                <p key={crypto.randomUUID()}>{dish}</p>
+              ))}
+            </div>
+
+            <div className="otherInfo">
+              <span>Ready In Minutes: {fullRecipe.readyInMinutes}min</span>
+            </div>
+            <h2>Summary</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(fullRecipe.summary),
+              }}
+            />
+            <p></p>
+          </article>
+          <a href={fullRecipe.sourceUrl} target="_blank">
+            Read More about the recipe
+          </a>
+          <button onClick={() => setFavoriteRecipe(fullRecipe)}>
+            Add to Favorite
+          </button>
+        </>
+      )}
     </>
   );
 };
--- src/store/useRecipeStore.js
+++ src/store/useRecipeStore.js
@@ -3,69 +3,84 @@ import { immer } from "zustand/middleware/immer";
 import { persist } from "zustand/middleware";
 import axios from "axios";
 
-export const useRecipeStore = create(persist(
-  immer((set, get) => ({
-    food: "",
-    setFood: (userInput) => set((state) => {state.food = userInput}),
-    recipes: [],
-    fullRecipe: {},
-    loading: false,
-    error: null,
-    apiKey: import.meta.env.VITE_API_KEY,
-    searchRecipe: async (userFood) => {
-      set((state) => {
-        state.loading = true;
-        state.error = null;
-        state.recipes = [];
-      });
-      const apiKey = get().apiKey;
-      const url = `https://api.spoonacular.com/recipes/complexSearch?query=${userFood}&number=5&apiKey=${apiKey}`;
-
-      try {
-        const response = await axios.get(url);
-        console.log(response);
-
+export const useRecipeStore = create(
+  persist(
+    immer((set, get) => ({
+      food: "",
+      setFood: (userInput) =>
         set((state) => {
-          state.recipes = response.data.results ?? [];
-          state.loading = false;
-          if (state.recipes.length === 0) throw new Error("Recipe Not Found");
-        });
-      } catch (err) {
+          state.food = userInput;
+        }),
+      recipes: [],
+      fullRecipe: {},
+      favoriteRecipe:[],
+      setFavoriteRecipe: (recipe) => set((state) => {state.favoriteRecipe.push(recipe)}),
+      loading: false,
+      error: null,
+      apiKey: import.meta.env.VITE_API_KEY,
+      searchRecipe: async (userFood) => {
         set((state) => {
-          state.error = err.message;
-          state.loading = false;
+          state.loading = true;
+          state.error = null;
+          state.recipes = [];
         });
-        console.log(err.message);
-      }
-    },
+        const apiKey = get().apiKey;
+        const url = `https://api.spoonacular.com/recipes/complexSearch?query=${userFood}&number=5&apiKey=${apiKey}`;
 
-    displayFullRecipes: async (id) => {
-      set((state) => {
-        state.loading = true;
-        state.error = null;
-        state.fullRecipe = {}
-      });
-    
-      const apiKey = get().apiKey;
-      const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`;
-      try {
-        const response = await axios.get(url);
-        const data = response?.data;
-        if(!data || Object.keys(data).length === 0) {
-          throw new Error("No recipe details found");
+        try {
+          const response = await axios.get(url);
+          console.log(response);
+
+          set((state) => {
+            state.recipes = response.data.results ?? [];
+            state.loading = false;
+            if (state.recipes.length === 0) throw new Error("Recipe Not Found");
+          });
+        } catch (err) {
+          set((state) => {
+            state.error = err.message;
+            state.loading = false;
+          });
+          console.log(err.message);
         }
-        console.log(response.data);
+      },
+
+      displayFullRecipes: async (id) => {
         set((state) => {
+          state.loading = true;
+          state.error = null;
+          state.fullRecipe = {};
+        });
+
+        const apiKey = get().apiKey;
+        const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`;
+        try {
+          const response = await axios.get(url);
+          const data = response?.data;
+          if (!data || Object.keys(data).length === 0) {
+            throw new Error("No recipe details found");
+          }
+          console.log(response.data);
+          set((state) => {
             state.fullRecipe = data;
             state.loading = false;
-        })
-      } catch (err) {
-        set((state) => {
-          state.error = err.message || "Something went wrong";
-          state.loading = false;
-        });
-      }
-    },
-    
-  }))
-));
+          });
+        } catch (err) {
+          set((state) => {
+            state.error = err.message || "Something went wrong";
+            state.loading = false;
+          });
+        }
+      },
+    })),
+    {
+      name: "recipe-storage",
+      partialize: (state) => ({
+        fullRecipe: state.fullRecipe,
+        recipes: state.recipes,
+        food: state.food,
+        favoriteRecipe: state.favoriteRecipe,
+      })
+    }
+  )
+);
