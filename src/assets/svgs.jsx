a87cb2cec7f8a943b1414f548954776ea38db20a
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-05-27 20:07:11 +0100

    Finished styling Recipe Card

--- src/assets/svgs.jsx
+++ src/assets/svgs.jsx
@@ -49,7 +49,7 @@ export const SearchIcon = () => {
 
 export const StarIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
       <path
         fill="#fff"
         d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useRecipeStore } from "../store/useRecipeStore.js";
 import { StarIcon } from "../assets/svgs.jsx";
+import chefImage from "/src/assets/chef.png";
 
 export const RecipeCard = () => {
   const food = useRecipeStore((state) => state.food);
@@ -15,7 +16,7 @@ export const RecipeCard = () => {
         {loading && <p>Loading...</p>}
         {error && <p>Error: {error}</p>}
         <div>
-          <h3 className="text-3xl font-bold">Healthy Recipe</h3>
+          <h3 className="text-3xl font-bold">Healthy Recipes</h3>
           <p className="flex justify-between">
             <span className="text-lg font-semibold text-orange">With Features</span>
             <span className="text-sm tracking-wider text-silver">See all</span>
@@ -23,23 +24,23 @@ export const RecipeCard = () => {
         </div>
         <article className="mt-8 grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-6">
           {recipes.map((recipe) => (
-            <div className="card-rounded card-shadow flex flex-col bg-white p-4" key={recipe.id}>
+            <div className="card-rounded card-shadow flex flex-col bg-white p-4 gap-4" key={recipe.id}>
               <img
                 className="rounded-full border-4 border-orange"
                 src={recipe.image}
                 alt={recipe.title}
               />
-              <p className="mt-4 text-lg font-bold text-dark">{recipe.title}</p>
+              <p className="text-lg font-bold text-dark">{recipe.title}</p>
               <div className="flex justify-between">
                 <span className="text-sm tracking-wider text-silver">{food}</span>
-                <div className= "gap-1 rounded-3xl px-2 py-0.5 bg-orange flex flex-row-reverse items-center justify-between">
+                <div className=" flex items-center justify-between gap-[0.13rem] rounded-3xl bg-orange px-1 py-0.5">
                   <StarIcon />
-                  <p className="text-[0.5rem] text-white">5.0</p>
+                  <p className="text-xs font-semibold text-white">5.0</p>
                 </div>
               </div>
 
               <Link
-                className="mt-auto"
+                className="mt-auto text-blue-500 underline hover:text-blue-800 transition duration-200"
                 to={`recipe/${recipe.id}`}
                 onClick={() => displayFullRecipes(recipe.id)}
               >
@@ -48,6 +49,15 @@ export const RecipeCard = () => {
             </div>
           ))}
         </article>
+
+        <div className="mt-10 rounded-lg bg-orange p-6 sm:max-w-xl sm:mx-auto">
+          <h2 className="text-3xl font-bold text-white">
+            Learn how to become a master chef right now
+          </h2>
+          <button className="mt-4 rounded-xl bg-white px-4 py-2 font-semibold text-orange">
+            Login
+          </button>
+        </div>
       </div>
     </section>
   );
