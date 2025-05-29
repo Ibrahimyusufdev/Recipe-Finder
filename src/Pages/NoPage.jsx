4110e273b88b8eaadf4e6ea017462ccf754a32ef
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-05-29 03:51:21 +0100

    restructure folder

--- src/Pages/NoPage.jsx
+++ src/Pages/NoPage.jsx
@@ -1,7 +1,8 @@
 export const NoPage = () => {
   return (
-    <div className="container mx-auto mt-6 px-4">
-      <h1>This page cannot be found</h1>
+    <div className="container mx-auto mt-6 px-4 text-center">
+       <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-lg">Page not found</p>
     </div>
   );
 };
